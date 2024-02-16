dd23cde6077155c14299cfa730e2e708fa09aafc
Gal Weiss <weiss_gal@yahoo.com>
2024-02-16 15:47:47 +0200

    added attempts and random cycles

--- assignment_problems/student_assignment.py
+++ assignment_problems/student_assignment.py
@@ -5,21 +5,49 @@ from datetime import datetime
 STUDENTS_NUM = 70
 GROUP_NUM = 7
 GROUP_SIZE = 10
+PREFERENCE_NUM = 5
 
 def rand_bool(probability=0.5):
     return random.random() < probability
 
+
+class PreferenceStrategy:
+    def get_preferences(self, preferences_num:int) -> list[int]:
+        raise NotImplementedError("Subclasses must implement this method")
+
+class IdenticalPreferenceStrategy(PreferenceStrategy):
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    def get_preferences(self, student, preferences_num):
+        return list(range(self.min, self.max))[:preferences_num]
+
+class GaussianPreferenceStrategy(PreferenceStrategy):
+    def __init__(self, min, max, std_dev=None):
+        self.min = min
+        self.max = max
+        self.std_dev = std_dev if std_dev is not None else (max - min) / 3
+    
+    def get_preferences(self, student, preferences_num):
+        preferences = []
+        while len(preferences) < preferences_num:
+            preference = int(random.gauss((self.min + self.max) / 2, self.std_dev))
+            preference = max(self.min, min(self.max, preference))
+            if preference not in preferences:
+                preferences.append(preference)  
+
+        return preferences
+
+PREFERENCE_STRATEGY = GaussianPreferenceStrategy(0, GROUP_NUM-1, 2)
+#PREFERENCE_STRATEGY = IdenticalPreferenceStrategy(0, GROUP_NUM)
+
 class Student:
-    def __init__(self, id, constraint_prob=0.5, preferences_num=3):
+    def __init__(self, id, constraint_prob=0.5, preferences_num:int=PREFERENCE_NUM):
         self.id = id
         self.constraints = [rand_bool(constraint_prob) for i in range(0, GROUP_NUM)]
-        self.preferences = []
-        possible_preferences = set(range(0, GROUP_NUM))
-        for i in range(0, preferences_num):
-            choise = random.choice(list(possible_preferences))
-            self.preferences.append(choise)
-            possible_preferences.remove(choise)
-
+        self.preferences = PREFERENCE_STRATEGY.get_preferences(self, preferences_num)
+        
     def __str__(self):
         return "Constraints: " + str(self.constraints) + "\nPreferences: " + str(self.preferences) + "\n"
     
@@ -39,6 +67,9 @@ class Assigment:
         
     def __str__(self):
         return str(self.assignments) + "\nLoss: " + str(self.loss)
+    
+    def get_results(self):
+        return self.assignments
 
     def check_assignment(self, student, group_id):
         return student.check_assignment(group_id) and self.empty_slots_per_group[group_id] > 0
@@ -83,7 +114,15 @@ class Assigment:
 
         return new_assignment
 
-    def get_all_neighbours(self):
+    # return unique hash for the assignment
+    def get_hash(self):
+        return "#".join([str(a) for a in self.assignments])
+
+    def get_all_neighbours(self, distance=1):
+        if distance != 1:
+            raise NotImplementedError("Only distance 1 is supported")
+
+        neighbours_hash = set()
         for i in range(0, STUDENTS_NUM):
             for j in range(1, STUDENTS_NUM):
                 if i == j:
@@ -94,6 +133,11 @@ class Assigment:
                 if neighbour is None:
                     continue
             
+                h = neighbour.get_hash()
+                if h in neighbours_hash:
+                    continue
+
+                neighbours_hash.add(h)
                 yield neighbour
 
     def get_best_neighbour(self):
@@ -106,18 +150,18 @@ class Assigment:
 
         return best_neighbour
     
-def main():        
 
-    CYCLE_NUM = 100_000
-    min_loss = 100 * STUDENTS_NUM
+def summary(assignment):
+    for i in range(PREFERENCE_NUM):
+        print(f"Preference {i}:", [assignment.get_results()[s.id] == s.preferences[i] for s in students].count(True))
+
+def get_best_random_assignment(cycles_num=1000):
+    # Find one assignment that satisfies all constraints and minimizes the loss
     best_assignment = None
-    start_time = datetime.now()
-    last_improvement_time = None
-
-    for cycle in range(0, CYCLE_NUM):
+    min_loss = 100 * STUDENTS_NUM
+    for cycle in range(0, cycles_num):
         #print(f"Cycle {cycle}...")
 
-        # Find one assignment that satisfies all constraints and minimizes the loss
         assignment = Assigment()
         while assignment.loss < min_loss:
             unassigned_students = assignment.get_unassigned_students()
@@ -137,37 +181,57 @@ def main():
             assignment.assign(student, group_id)
             #print(f"Assigned student {student_id} to group {group_id} with loss {assignment.loss}")
 
-            
-            
         if len(assignment.get_unassigned_students())==0 and assignment.loss < min_loss:
-
-            print("New best assignment found, checking its neighbours...")
-            if True:
-                best_neighbour = assignment.get_best_neighbour()
-                while best_neighbour is not None:
-                    assignment = best_neighbour
-                    best_neighbour = assignment.get_best_neighbour()
-                    print(f"New best assignment found with loss {assignment.loss}")
-
-            improvement = min_loss - assignment.loss
+            print(f"New best assignment found with loss {assignment.loss}")
             min_loss = assignment.loss
             best_assignment = assignment
-            print(f"New best assignment found with loss {min_loss}")
-            if last_improvement_time is not None:
-                try:
-                    print(f"Improvement rate (per second): {improvement / (datetime.now() - last_improvement_time).total_seconds()}")
-                except ZeroDivisionError:
-                    # this is fine for the initial phase of the algorithm when many imrpovements are made in a short time
-                    pass
-            last_improvement_time = datetime.now()
+
+    return best_assignment
+
+def main():        
+    ATTEMPTS_NUM = 10
+    RANDOM_CYCLES_NUM = 1_000
+    best_assignment = None
+    start_time = datetime.now()
+    
+    for i in range(ATTEMPTS_NUM):
+        print(f"Attempt {i}...")
+        attempt_best_assignment = None
+        assignment = get_best_random_assignment(RANDOM_CYCLES_NUM)
+        if assignment is None:
+            print(f"Attempt {i} failed")
+
+        print("Initial assignment found with loss", assignment.loss)
+        print("assignment", assignment) # XXX debug
+        print("Attempting to improve the assignment...")
+            
+        best_neighbour = assignment.get_best_neighbour()
+        while best_neighbour is not None:
+            assignment = best_neighbour
+            best_neighbour = assignment.get_best_neighbour()
+            print(f"New neighbour assignment found with loss {assignment.loss}")
+
+            attempt_best_assignment = assignment
+            
+        if attempt_best_assignment is None:
+            print("Attempt to improve the assignment failed")
+            attempt_best_assignment = assignment
+
+        if best_assignment is None or attempt_best_assignment.loss < best_assignment.loss:
+            best_assignment = attempt_best_assignment
 
 
     time_sec = (datetime.now() - start_time).total_seconds()
-    print(f"Finished {CYCLE_NUM} cycles")
+    print(f"Finished {ATTEMPTS_NUM} attempts of up to {RANDOM_CYCLES_NUM} random cycles cycles")
     print(f"Time taken: {time_sec} seconds")
-    print(f"Average time per cycle: {time_sec/CYCLE_NUM} seconds")
-    print(f"Best assignment found with loss {min_loss}:\n{best_assignment}")
+    print(f"Average time per attempt: {time_sec/ATTEMPTS_NUM} seconds")
+    print(f"Best assignment found with loss {best_assignment.loss}:\n{best_assignment}")
+    summary(best_assignment)
+
 
 if __name__ == "__main__":
+
+
+
     #cProfile.run('main()')
     main()
